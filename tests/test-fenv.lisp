d046dfb70b65740cf698d28b7844a40427f55884
Torsten Anders <torsten.anders@beds.ac.uk>
2017-08-13 23:26:59 +0200

    Updated tests

--- tests/test-fenv.lisp
+++ tests/test-fenv.lisp
@@ -22,7 +22,7 @@
 ;            (funcall (fenv #'identity :min 0.3 :max 0.7)
 ;                     (* i 0.1)))))
 
-
+#|
 ;; depends on Opusmodus
 (defun plotter (data &optional (number 100))
   (list-plot (cond ((fenv? data) (fenv->vector data number))
@@ -30,72 +30,72 @@
                     (mapcar #'(lambda (xs) (fenv->vector xs number)) data))
                    (T data))
 	     :join-points T :point-radius 2))
-
+|#
 
 ;; examples
 
 
-(plotter (make-fenv #'sin :min (- pi) :max pi))
+(v (make-fenv #'sin :min (- pi) :max pi))
 
-(plotter
+(v
   (funcs->fenv (list #'identity
 		    #'identity
 		    #'identity)
 	      :min 1 :max 5))
 
-(plotter (fenv-seq (make-fenv #'sin :max pi)
+(v (fenv-seq (make-fenv #'sin :max pi)
 	   0.1 (make-fenv #'sin :max pi)
 	   0.4 (make-fenv #'sin :max pi)))
 
-(plotter (osciallator (make-fenv #'identity) 5))
+(v (osciallator (make-fenv #'identity) 5))
 	       
   
-(plotter (linear-fenv (0 1) (0.2 3) (0.7 2) (1 -1)))
+(v (linear-fenv (0 1) (0.2 3) (0.7 2) (1 -1)))
 
-(plotter (linear-fenv (0 0) (1 2)))
+(v (linear-fenv (0 0) (0.7 -1) (1 2)))
 
-(plotter (sin-fenv (0 1) (0.2 5) (0.3 -2) (0.8 7) (1 1)))
+(v (sin-fenv (0 1) (0.2 5) (0.3 -2) (0.8 7) (1 1)))
 
-(plotter (sin-fenv (0 1) (0.2 0) (0.3 1) (0.8 0) (1 1)))
+(v (sin-fenv (0 1) (0.2 0) (0.3 1) (0.8 0) (1 1)))
 
-(plotter (sin-fenv1 (0 1) (0.2 5) (0.3 -2) (0.8 7) (1 1)))
+(v (sin-fenv1 (0 1) (0.2 5) (0.3 -2) (0.8 7) (1 1)))
       
-(plotter (sin-fenv1 (0 1) (0.2 0) (0.3 1) (0.8 0) (1 1)))
+(v (sin-fenv1 (0 1) (0.2 0) (0.3 1) (0.8 0) (1 1)))
 
 
 ;; !! not OK yet
-(plotter (expon-fenv (0 1) (0.2 5) (0.3 2) (0.8 7) (1 1)))
+(v (expon-fenv (0 1) (0.2 5) (0.3 2) (0.8 7) (1 1)))
 
 
-(plotter (combine-fenvs #'+
+(v (combine-fenvs #'+
 		    10
 		    (linear-fenv (0 0) (1 2))
 		    (make-fenv #'sin :max (* pi 2))
 		    (make-fenv #'sin :max pi)))
 
 
-(plotter (linear-fenv (0 0) (0.7 2) (1 1)))
-(plotter (reverse-fenv (linear-fenv (0 0) (0.7 2) (1 1))))
+(v (linear-fenv (0 0) (0.7 2) (1 1)))
+(v (reverse-fenv (linear-fenv (0 0) (0.7 2) (1 1))))
 
-(plotter (saw 3))
-(plotter (saw1 3 :amplitude 3 :offset 1))
+(v (saw-fenv 3))
+(v (saw1-fenv 3 :amplitude 3 :offset 1))
 
-(plotter (triangle 3 :amplitude 3 :offset 1))
+(v (triangle-fenv 3 :amplitude 3 :offset 1))
 
-(plotter (square 3 :amplitude 3 :offset 1))
+(v (square-fenv 3 :amplitude 3 :offset 1))
 
-(plotter (steps 1 2 3 2 1))
+(v (steps-fenv 1 2 3 2 1))
 
-(plotter (random-steps 20 :min-y -10 :max-y -1)))
+(v (random-steps-fenv 20 :min-y -10 :max-y -1)))
 
-(plotter (random-steps 20 :min-y -1 :max-y 3))
+(v (random-steps-fenv 20 :min-y -1 :max-y 3))
 
-(plotter (combine-fenvs #'*
+(v (combine-fenvs #'*
 		    (triangle 7)
 		    (linear-fenv (0 0) (0.3 1) (1 0))))
 
 
-(plotter 
+(v 
  (scale-fenv 
   (make-fenv #'(lambda (x)
 		(let ((exp 1000))
@@ -104,7 +104,7 @@
 
 ;;
 (let ((fenv (make-fenv #'sin :max pi)))
-  (plotter (mapcar #'(lambda (fenv2)
+  (v (mapcar #'(lambda (fenv2)
 		    (waveshape fenv fenv2))
 		(list
 		 (make-fenv #'identity) ; no waveshaping
@@ -114,9 +114,9 @@
 			   :max (* pi 0.5))
 		 fenv))))
 
-(plotter (rising-expon-fenv 10))
+(v (rising-expon-fenv 10))
 
-(plotter 
+(v 
  (scale-fenv (rising-expon-fenv 10)
 	    3 0.1))
 
