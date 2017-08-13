05ddb68ea092a748167b9d974cd27bffc6267586
Torsten Anders <torsten.anders@beds.ac.uk>
2017-08-13 23:26:39 +0200

    Added constant-segements-fenv-fn etc

--- sources/macros.lisp
+++ sources/macros.lisp
@@ -14,6 +14,8 @@
   "Defines an fenv which interpolates the given points by a linear function. Expects a list of x-y-pairs as (0 y1) ... (1 yn)"
   `(linear-fenv-fn ',points))
 
+; (v (linear-fenv (0 0) (0.5 1) (0.7 -1) (1 2)) 10)
+
 (defun sin-fenv-fn (points)
   "Returns a fenv which interpolates the given points by a sin function. Using only the intervals [0,pi/2] and [pi, 3pi/4] results in edges. Expects a list of x-y-pairs as (0 y1) ... (1 yn)."
   (points->fenv #'(lambda (x1 y1 x2 y2)
@@ -60,3 +62,26 @@ BUG: Definition wrong -- slope completely bogus!"
 (defmacro expon-fenv (&rest points)
   "Defines an envelope described be exponential functions. Expects a list of x-y-pairs as (0 y1) ... (1 yn). y values can not be negative."
   `(expon-fenv-fn ',points))
+
+
+(defun constant-segements-fenv-fn (points)
+  (points->fenv #'(lambda (x1 y1 x2 y2)
+                    (declare (ignore y2))
+                    (make-fenv1 #'(lambda (x) 
+                                    (declare (ignore x)) 
+                                    ;; ensure result is a float
+                                    ; (* y1 1.0)
+                                    y1
+                                    )
+                                :min x1 :max x2))
+                points))
+
+(defmacro constant-segements-fenv (&rest points)
+  "Defines an fenv that consists of constant segments, like a steps function or sample and hold. Expects a list of x-y-pairs as (0 y1) ... (1 yn).
+
+NOTE: The last y value is ignored."
+  `(constant-segements-fenv-fn ',points))
+ 
+; (v (constant-segements-fenv (0 0) (0.5 1) (0.7 -1) (1 2)) 10)
+
+
